6d595b2c4e8e126a120453aad82d5c31b88f7b07
Pavel Martsinovich <pavel.m@hypervsn.com>
2023-11-14 14:31:33 +0300

    UpdateBuffer: move statistic to this func, refactor

--- src/tcpServer_UpdateBuffer.c
+++ src/tcpServer_UpdateBuffer.c
@@ -2,9 +2,31 @@
 #include "libtcp.h"
 
 int UpdateBuffer(struct tcpServer *s, void *newBuffer, size_t newBufferSize) {
-  while(s->sent < s->clientCount) {
-    usleep(1000);
+  static struct timespec prev = {.tv_sec = 0, .tv_nsec = 0};
+  int sleepCount = 0;
+  int usleepValue = 100;
+  while(s->sent < s->clientCount && s->clientCount > 0) {
+    sleepCount++;
+    usleep(usleepValue);
   }
+  //if (sleepCount > 0) {
+  struct timespec now;
+  int64_t diff = -1;
+  if (clock_gettime(CLOCK_MONOTONIC, &now) == 0) {
+    diff = now.tv_nsec - prev.tv_nsec + (now.tv_sec - prev.tv_sec) * 1000000000L;
+    prev.tv_sec = now.tv_sec;
+    prev.tv_nsec = now.tv_nsec;
+  }
+    fprintf(
+      stderr,
+      "UpdateBuffer idx: %2d, sent: %2d, clientCount: %2d, waitFor: %9d, diff: %011ld\n",
+      s->sentIdx,
+      s->sent,
+      s->clientCount,
+      sleepCount * usleepValue,
+      diff
+    );
+  //}
   pthread_mutex_lock(&s->mutex);
   s->sentIdx = (s->sentIdx + 1) % 16;
   s->buffer = newBuffer;
